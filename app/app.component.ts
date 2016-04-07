4da1ea5904b11d6dfe707321142b86298e02bfaa
Echo Yu <eyu@AT-1060.local>
2016-04-07 16:42:05 -0400

    add routing on app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,15 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {InstasComponent} from './instas.component';
 
 @Component({
     selector: 'insta-app',
-    templateUrl: 'app/app.component.html'
+    templateUrl: 'app/app.component.html',
+    directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+  {path: '/', component: InstasComponent, useAsDefault: true}
+])
 export class AppComponent {
     title = 'Insta gallery';
     hashtag : string;
---
+++ app/instas.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+import {Router} from 'angular2/router';
+
+@Component({
+    selector: 'my-instas',
+    templateUrl: 'app/instas.component.html'
+})
+export class InstasComponent {
+  
+}
