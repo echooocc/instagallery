b78b240e9601293b76078cf7efd9ff9c788cebe2
Echo Yu <echo2go@gmail.com>
2016-04-12 02:28:45 -0400

    update the image views on index

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,26 @@
 import {Component, Input} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
-import {InstasComponent} from './instas.component';
 import {InstaService} from './insta.service';
-import { Insta } from './insta';
 
 @Component({
     selector: 'insta-app',
     templateUrl: 'app/app.component.html',
-    directives: [ROUTER_DIRECTIVES, InstasComponent],
+    directives: [ROUTER_DIRECTIVES],
     providers: [InstaService, HTTP_PROVIDERS]
 })
 
 export class AppComponent{  
     title = 'Insta gallery';  
     constructor(private _instaService: InstaService) { }
-    instas: Insta[];
-    search(tag: string) { 
-       return this.instas = this._instaService.getInstas(tag);
+    public instas:any;
+    search(tag: string) {
+        this._instaService.getInstas(tag)
+            .subscribe(
+                    res => {this.instas = res},
+                    err => console.error(err),
+                    () => console.log('success! '+this.instas)
+            );
+        console.log(this.instas);
     }
 }
--- app/insta.service.ts
+++ app/insta.service.ts
@@ -3,20 +3,12 @@ import { Http } from 'angular2/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map'; //add this to avoid "this._http.get(...).map is not a function" error
 
-import { Insta } from './insta';
-
 @Injectable()
 export class InstaService{
-    instas: Insta[];
     constructor(private _http: Http) { }
     getInstas(tag: string) { 
        return this._http.get('http://localhost:3000/hashtag/'+tag)
-        .map(res => res.json().data)
-        .subscribe(
-            res => {this.instas = res},
-            err => console.error(err),
-            () => console.log('success! '+this.instas)
-        );
+        .map(res => res.json().data);
     }
 }
 
